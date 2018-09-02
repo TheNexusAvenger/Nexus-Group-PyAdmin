0dc42c49d05dfaf320b6ecffaa9c303dfa323682
TheNexusAvenger <zpcook@googlemail.com>
2018-09-02 12:09:54 -0400

    Initial commit

---
+++ GroupAdminBot.py
@@ -0,0 +1,303 @@
+"""
+TheNexusAvenger
+Automated bot for deleting posts by users who have left.
+
+Roblox HTTP(S) Documentation:
+https://groups.roblox.com/docs
+https://auth.roblox.com/docs
+"""
+
+import re
+import requests
+import sys
+import time
+import threading
+import json
+import os,ntpath
+
+BOT_SETTINGS_JSON_LOCATION = ntpath.split(os.path.realpath(__file__))[0] + "/BotSettings.json"
+
+MAX_POSTS_PER_REQUEST = "100"
+GLOBAL_USER_BAN_LIST = []
+GLOBAL_POST_BLACK_LIST = []
+GROUP_BOTS = []
+
+LIGHT_SCAN_LOOP_DELAY_IN_SECONDS = 30
+LIGHT_SCAN_LOOP_ITERATIONS = 10
+
+
+
+# Creates a bot class.
+class RobloxBot(object):
+	# Initializes the class.
+	def __init__(self):
+		# Creates a session.
+		self.session = requests.session()
+
+		# Create the headers.
+		headers = {"User-Agent":"Roblox/WinInet"}
+		self.session.headers.update(headers)
+
+		# Set the place holder rules.
+		self.botGroupRank = 0
+		self.removePostsFromList = False
+		self.removePostsOnLeave = False
+		self.removeMembers = False
+		self.postBlackList = []
+		self.userBanList = []
+
+	# Adds a site header to the list of headers.
+	def addSiteKeyToHeaders(self,headers,url):
+		response = self.session.get("https://www.roblox.com/",timeout=10)
+		csrf_token = response.text.split("Roblox.XsrfToken.setToken('")[-1].split("');")[0]
+
+		# Add the headers.
+		headers["X-CSRF-TOKEN"] = csrf_token
+		headers["Referer"]  = url
+
+		# Return the headers.
+		return headers
+
+	# Returns if a posts should be deleted.
+	def shouldPostBeDeleted(self,postData):
+		posterRank = int(postData["poster"]["role"]["rank"])
+		postBody = postData["body"]
+
+		if posterRank == 0:
+			# Handle the poster not being in the group.
+			if self.removePostsOnLeave:
+				return True
+		elif posterRank < self.botGroupRank:
+			# Handle blacklist posts.
+			if self.removePostsFromList:
+				for pattern in GLOBAL_POST_BLACK_LIST:
+					if re.search(pattern,postBody) != None:
+						return True
+
+		# Return false (don't delete).
+		return False
+
+	# Signs the bot in and returns if it was successful.
+	def SignIn(self,username,password):
+		# Assemble the arguments.
+		arguments = {
+			"username": username,
+			"password": password,
+		}
+
+		# Send the request and return if it was successful.
+		response = self.session.post('https://api.roblox.com/v2/login',headers=self.addSiteKeyToHeaders({},"https://www.roblox.com/"),data=arguments)
+		return response.status_code < 400
+
+	# Sets the bot rules for deleting posts.
+	def SetPostDeletionRules(self,botGroupRank,removePostsFromList,removePostsOnLeave,postBlackList):
+		self.botGroupRank = botGroupRank
+		self.removePostsFromList = removePostsFromList
+		self.removePostsOnLeave = removePostsOnLeave
+		self.postBlackList = postBlackList
+
+		for pattern in GLOBAL_POST_BLACK_LIST:
+			if not pattern in self.postBlackList:
+				self.postBlackList.append(pattern)
+
+	# Sets the bot rules for exiling users.
+	def SetExileRules(self,botGroupRank,removeMembers,userBanList):
+		self.botGroupRank = botGroupRank
+		self.removeMembers = removeMembers
+		self.userBanList = userBanList
+
+		for userId in GLOBAL_USER_BAN_LIST:
+			if not userId in self.userBanList:
+				self.userBanList.append(userId)
+
+	# Returns 100 posts. Accepts a cursor to move pages.
+	def GetPosts(self,groupId,cursor = None):
+		# Create the base URL.
+		requestUrl = "https://groups.roblox.com/v2/groups/" + str(groupId) + "/wall/posts?sortOrder=Desc&limit=" + MAX_POSTS_PER_REQUEST
+		headers = self.addSiteKeyToHeaders({},"https://www.roblox.com/My/Groups.aspx?gid=" + str(groupId))
+		if cursor is not None:
+			requestUrl += "&cursor=" + cursor
+
+		# Send the request.
+		response = self.session.get(requestUrl,headers=headers)
+
+		# Return the response.
+		return response.json()
+
+	# Deletes a group wall post. Returns if it was successful.
+	def DeletePost(self,groupId,postId):
+		requestUrl = "https://groups.roblox.com/v1/groups/" + str(groupId) + "/wall/posts/" + str(postId)
+		headers = self.addSiteKeyToHeaders({},"https://www.roblox.com/My/Groups.aspx?gid=" + str(groupId))
+
+		# Send the request and return if it was successful.
+		response = self.session.delete(requestUrl,headers=headers)
+
+		# Display an error (if any) and return the status.
+		if response.status_code < 400:
+			return True
+		else:
+			print("An error occurred (with RobloxBot.DeletePost): " + response.text)
+			return False
+
+	# Exiles a user. Assumes the user is in the group.
+	def ExileUser(self,groupId,userId):
+		requestUrl = "https://groups.roblox.com/v1/groups/"  + str(groupId) + "/users/" + str(userId)
+		headers = self.addSiteKeyToHeaders({}, "https://www.roblox.com/My/Groups.aspx?gid=" + str(groupId))
+
+		# Send the request and return if it was successful.
+		response = self.session.delete(requestUrl,headers=headers)
+
+		# Display an error (if any) and return the status.
+		if response.status_code < 400:
+			return True
+		else:
+			print("An error occurred (with RobloxBot.DeletePost): " + response.text)
+			return False
+
+	# Gets the user's rank in the group.
+	def GetUserRank(self,groupId,userId):
+		requestUrl = "https://www.roblox.com/Game/LuaWebService/HandleSocialRequest.ashx?method=GetGroupRank&playerid=" + str(userId) + "&groupid=" + str(groupId)
+
+		# Send the request.
+		response = self.session.get(requestUrl) #, headers=headers)
+
+		# Display an error (if any) and return the role
+		if response.status_code < 400:
+			return int(response.text[22:-8])
+		else:
+			print("An error occurred (with RobloxBot.DeletePost): " + response.text)
+			return 0
+
+	# Exiles banned users from the group.
+	def ExileUsers(self,groupId):
+		# Return if exiling is disabled.
+		if not self.removeMembers:
+			return
+
+		# Go through the banned users and remove them if they are in the group.
+		for userId in self.userBanList:
+			userRank = self.GetUserRank(groupId,userId)
+			if userRank > 0 and userRank < self.botGroupRank:
+				self.ExileUser(groupId,userId)
+
+	# Deletes group wall posts for the current cursor.
+	def DeletePosts(self,groupId,runNextPages = False,cursor = None):
+		# Get the posts.
+		posts = self.GetPosts(groupId,cursor)
+
+		if "data" in posts:
+			# Iterate through the posts.
+			for post in posts["data"]:
+				if self.shouldPostBeDeleted(post) == True:
+					postId = post["id"]
+					self.DeletePost(groupId,postId)
+		else:
+			raise Exception("An error occurred (with RobloxBot.DeletePosts): " + str(posts))
+
+		# Move to the next posts (if needed).
+		nextCursor = posts["nextPageCursor"]
+		if nextCursor is not None and runNextPages == True:
+			self.DeletePosts(groupId,True,nextCursor)
+
+
+
+# Creates a bot and go through the first MAX_POSTS_PER_REQUEST posts, and exiles users.
+def RunLightScanForGroup(groupData):
+	# Get the bot info.
+	groupId = groupData["GroupId"]
+	botGroupRank = groupData["GroupRank"]
+	botUsername = groupData["Username"]
+	botPassword = groupData["Password"]
+	removePostsFromList = groupData["RemovePostsFromList"]
+	removePostsOnLeave = groupData["RemovePostsOnLeave"]
+	removeMembers = groupData["RemoveMembers"]
+	userBanList = groupData["UserBanList"]
+	postBlackList = groupData["PostBlackList"]
+
+	# Handle the bot actions.
+	if removePostsFromList or removePostsOnLeave or removeMembers:
+		bot = RobloxBot()
+		bot.SignIn(botUsername,botPassword)
+		bot.SetPostDeletionRules(botGroupRank,removePostsFromList,removePostsOnLeave,postBlackList)
+		bot.SetExileRules(botGroupRank,removeMembers,userBanList)
+		bot.ExileUsers(groupId)
+		bot.DeletePosts(groupId,False)
+
+# Creates a bot and go through all the posts, and exiles users.
+def RunFullScanForGroup(groupData):
+	# Get the bot info.
+	groupId = groupData["GroupId"]
+	botGroupRank = groupData["GroupRank"]
+	botUsername = groupData["Username"]
+	botPassword = groupData["Password"]
+	removePostsFromList = groupData["RemovePostsFromList"]
+	removePostsOnLeave = groupData["RemovePostsOnLeave"]
+	removeMembers = groupData["RemoveMembers"]
+	userBanList = groupData["UserBanList"]
+	postBlackList = groupData["PostBlackList"]
+
+	# Handle the bot actions.
+	if removePostsFromList or removePostsOnLeave or removeMembers:
+		bot = RobloxBot()
+		bot.SignIn(botUsername,botPassword)
+		bot.SetPostDeletionRules(botGroupRank,removePostsFromList,removePostsOnLeave,postBlackList)
+		bot.SetExileRules(botGroupRank,removeMembers,userBanList)
+		bot.ExileUsers(groupId)
+		bot.DeletePosts(groupId,True)
+
+# Runs a light scan on all groups.
+def RunLightScan():
+	for groupData in GROUP_BOTS:
+		RunLightScanForGroup(groupData)
+
+# Runs a full scan on all groups.
+def RunFullScan():
+	for groupData in GROUP_BOTS:
+		RunFullScanForGroup(groupData)
+
+# Calls a function and prints error checking.
+def pcall(function,*parameters):
+	try:
+		function(*parameters)
+	except Exception as e:
+		print("An error occurred: " + str(e))
+
+# Calls a function using pcall in a thread.
+def threadedPCall(function,*parameters):
+	threading.Thread(target=function,args=parameters).start()
+
+# Loads the current settings.
+def LoadJSONSettings(settingsFileLocation):
+	with open(settingsFileLocation) as file:
+		jsonData = json.load(file)
+
+		# Set the globals.
+		global MAX_POSTS_PER_REQUEST,GLOBAL_USER_BAN_LIST
+		global GLOBAL_POST_BLACK_LIST,GROUP_BOTS
+		global LIGHT_SCAN_LOOP_DELAY_IN_SECONDS,LIGHT_SCAN_LOOP_ITERATIONS
+
+		MAX_POSTS_PER_REQUEST = jsonData["MaxPostsPerRequest"]
+		GLOBAL_USER_BAN_LIST = jsonData["GlobalUserBanList"]
+		GLOBAL_POST_BLACK_LIST = jsonData["GlobalPostBlackList"]
+		GROUP_BOTS = jsonData["GroupBots"]
+		LIGHT_SCAN_LOOP_DELAY_IN_SECONDS = jsonData["LightScanLoopDelayInSeconds"]
+		LIGHT_SCAN_LOOP_ITERATIONS = jsonData["LightScanLoopIterations"]
+
+
+
+# Runs the script if it is directly called.
+if __name__ == '__main__':
+	# Loads the JSON settings.
+	LoadJSONSettings(BOT_SETTINGS_JSON_LOCATION)
+
+	# Run the script based on command line arguments.
+	if "FullScan" in sys.argv:
+		threadedPCall(RunFullScan)
+	elif "LightScan" in sys.argv:
+		if "Loop" in sys.argv:
+			# Run the loop 10 times.
+			for _ in range(0,LIGHT_SCAN_LOOP_ITERATIONS):
+				threadedPCall(RunLightScan)
+				time.sleep(LIGHT_SCAN_LOOP_DELAY_IN_SECONDS)
+		else:
+			threadedPCall(RunLightScan)
---
+++ README.md
@@ -0,0 +1,59 @@
+# Nexus-Group-PyAdmin
+Nexus Group PyAdmin is a Roblox bot used for automating group actions. The actions it can handle include:
+*  Removing posts from people who have left.
+*  Removing posts that are in a blacklist, including regular expression entries.
+*  Exiling users that are banned from the group.
+
+To use all the included features, the bot must have the following:
+*  Posts
+    * View group wall
+    * Delete group wall posts
+*  Members
+    * Kick lower-ranked members
+
+It is recommended that only the needed permissions are enabled when they come up. If there is a data breach, the bot may become compromised and start removing members randomly.
+
+# Dependancies
+The bot currently requires the "requests" library. If you don't have requests, and are able to control the Python install, requests can be installed using the following:
+
+```
+pip install requests
+```
+
+If you aren't able to control your install, like in a shared hosting environment, you will need to down the approriate requests folder from the [requests repository](https://github.com/requests/requests/releases).
+
+# Setup
+Setup of the bot requires an environment with a Python install. Python 2 and Python 3 both work as long as requests is included (see Dependancies). It is recommended to use a dedicated host, as apposed to a spare computer, to ensure it is always online. The bot is intended to be run using the command line with the following commands:
+
+```
+python LOCATION/GroupAdminBot.py LightScan
+```
+Goes through the entire ban list and the first MaxPostsPerRequest (see BotSettings.json).
+
+```
+python LOCATION/GroupAdminBot.py LightScan Loop
+```
+Runs the same behavior as LightScan, but performs if every LightScanLoopDelayInSeconds for LightScanLoopIterations times. This is intended for hosts that limit the intervals between Cron jobs. The repository assumes 5 minutes being the minimum.
+
+```
+python LOCATION/GroupAdminBot.py FullScan
+```
+Goes through the entire ban list and all of the group wall posts. Full scans should be run rarely (like once an hour) because of the amount of requests created, and LightScan typically gets most of important messages. Loop isn't implemented for this.
+
+# BotSettings.json
+BotSettings.json stores the settings for running the bot as a JSON file. The following parameters include:
+*  MaxPostsPerRequest - the amount of posts to grab in each request. It must be "10", "25", "50", or "100".
+*  GlobalUserBanList - A list of the user ids of banned users. All bots use this list.
+*  GlobalPostBlackList - A list of blacklisted phrases in the group wall. All bots use this list. Regular expressions can be used.
+*  LightScanLoopDelayInSeconds - Delay between running the LightScan in a loop.
+*  LightScanLoopIterations - Amount of times bot will loop in LightScan.
+*  GroupBots - A list of bots handled by the host, including the following for each bot:
+    * GroupId - Group id to handle.
+    * GroupRank - Rank in the group as a number. This will prevent the bot sending requests to delete posts of higher ranks.
+    * Username - Username of the bot. This is used for logging in.
+    * Password - Password of the bot. This is used for logging in. A long, random password is recommended.
+    * RemovePostsFromList - A bool that determines if the bot should remove blacklisted posts.
+    * RemovePostsOnLeave - A bool that determines if the bot should remove posts from users that have left.
+    * RemoveMembers - A bool that determines if the bot should exile banned users.
+    * UserBanList - Same as GlobalUserBanList, but only applies for that group.
+    * PostBlackList - Same as GlobalPostBlackList, but only applies for that group.
